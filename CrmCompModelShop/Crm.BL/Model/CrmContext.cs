816604a83028789275101b9ec319ff244f060f1f
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-25 22:14:45 +0300

    EF wired up. CRM context created Migrations added.

---
+++ CrmCompModelShop/Crm.BL/Model/CrmContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.BL.Model
+{
+    public class CrmContext : DbContext
+    {
+
+        public CrmContext() : base("ConnectionStringCrmShopTest") { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cheque> Cheques { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+
+
+    }
+}
